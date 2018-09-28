ce0adaf4131e4d9b1018e163e7d90463401968d6
Norcux OS <NorcuxOS@protonmail.com>
2018-09-28 18:32:58 +0200

    Add desktop icons and improve audio volume fix script

--- live-iso/airootfs/usr/bin/fixaudiovolume.sh
+++ live-iso/airootfs/usr/bin/fixaudiovolume.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 pactl set-sink-volume 0 100%
 pactl set-sink-mute 0 0
 pactl set-sink-input-volume 0 100%
 pactl set-sink-input-mute 0 0
+rm .config/autostart/fixaudiovolume.desktop
---
+++ live-iso/airootfs/usr/bin/movedesktopfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+desktoppath=$(xdg-user-dir DESKTOP)
+mv ~/{Home.desktop,trash.desktop} "$desktoppath"
+rm .config/autostart/movedesktopfiles.desktop
