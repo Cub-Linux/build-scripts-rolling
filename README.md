f39668e9bafc61260d403a3dd33cce0a0da97e68
Norcux OS <NorcuxOS@protonmail.com>
2018-09-30 11:54:12 +0200

    Clean up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Build Scripts for Norcux OS Rolling
 
-To build an ISO image execute the following in the "live-iso" folder:
+To build an ISO image execute the following in the `live-iso` folder:
 ```
 sudo ./build.sh -V Alpha_0.1 -R 1 -v
 ```
--- live-iso/airootfs/root/customize_airootfs.sh
+++ live-iso/airootfs/root/customize_airootfs.sh
@@ -1,38 +1,81 @@
 #!/bin/bash
 
-set -e -u
+function setUmask() {
+    # Set umask
+    set -e -u
+    umask 022
+}
 
-sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
-locale-gen
+function genLocale() {
+    # Set locales
+    sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+    locale-gen
+}
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+function setTimeZoneAndClock() {
+    # Set timezone
+    ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-useradd -m -G "wheel,audio,network" -s /bin/bash defaultuser
+    # Set clock to UTC
+    hwclock --systohc --utc
+}
 
-usermod -s /usr/bin/bash root
-cp -aT /etc/skel/ /home/defaultuser/
-cp -aT /etc/skel/ /root/
-mkdir -p /home/defaultuser/Desktop/
-cp /usr/share/applications/calamares.desktop /home/defaultuser/Desktop/
-cp /usr/share/applications/calamares.desktop /home/defaultuser/.config/autostart/
-chmod +x /home/defaultuser/Desktop/calamares.desktop
-chmod +x /home/defaultuser/.config/autostart/calamares.desktop
-rm -rf /home/defaultuser/.config/autostart/welcome-screen.desktop
-chmod 750 /root
-chown -R defaultuser:users /home/defaultuser/
-chfn -f "Default User" defaultuser
-echo "defaultuser:default"|chpasswd
+function configRootUser() {
+    # Configure root user
+    usermod -s /usr/bin/bash root
+    chmod 750 /root
+    cp -aT /etc/skel/ /root/
+}
 
-#sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
-sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
-sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+function createLiveUser() {
+    # Add live user
+    useradd -m -G "wheel,audio,network" -s /bin/bash defaultuser
+    chown -R defaultuser:users /home/defaultuser/
+    chfn -f "Default User" defaultuser
+    echo "defaultuser:default" | chpasswd
+}
 
-sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+function configureLiveUser() {
+    # Configure live user
+    mkdir -p /home/defaultuser/Desktop/
+    cp /usr/share/applications/calamares.desktop /home/defaultuser/Desktop/
+    cp /usr/share/applications/calamares.desktop /home/defaultuser/.config/autostart/
+    chmod +x /home/defaultuser/Desktop/calamares.desktop
+    chmod +x /home/defaultuser/.config/autostart/calamares.desktop
+    rm -rf /home/defaultuser/.config/autostart/welcome-screen.desktop
+}
 
-sed -i '/#TEMPORARY/,$d' /etc/pacman.conf
+function miscConfigs() {
+    # Misc configs
+    sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+    sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+    sed -i '/#TEMPORARY/,$d' /etc/pacman.conf
+}
 
-systemctl enable pacman-init.service sddm NetworkManager
+function systemdAntiDumbasseryHack() {
+    # systemd anti-dumbassery hack
+    echo 'This is systemd anti-dumbassery hack. See systemd-update-done.service(8).' | tee /etc/.updated > /var/.updated
+}
 
-echo 'This is systemd anti-dumbassery hack. See systemd-update-done.service(8).' | tee /etc/.updated > /var/.updated
+function fixHibernate() {
+    # Fix hibernate
+    sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+    sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+    sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+}
+
+function enableServices() {
+    # Enable services
+    systemctl enable pacman-init.service sddm NetworkManager
+}
+
+setUmask
+genLocale
+setTimeZoneAndClock
+configRootUser
+createLiveUser
+configureLiveUser
+miscConfigs
+systemdAntiDumbasseryHack
+fixHibernate
+enableServices
